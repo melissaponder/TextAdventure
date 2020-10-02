48c7b5ec18033527e8ff7609b1c719e5db0bb941
Melissa Ponder <mponder71186@gmail.com>
2020-10-02 12:55:48 -0500

    Added some flare to help with user readability.

--- IslandAdventure/Program.cs
+++ IslandAdventure/Program.cs
@@ -14,6 +14,7 @@ namespace IslandAdventure
             Console.WriteLine("It looks like you have 2 options...");
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine("You can either build your own hut or journey into the jungle and find a cave.");
+            Console.WriteLine(".............................................................................");
             Choice1();
         }
 
@@ -27,41 +28,57 @@ namespace IslandAdventure
                 Console.WriteLine("Uh oh! Looks like the cave you found was already home to a wild animal!");
                 Console.WriteLine("It attacked you!");
                 Console.WriteLine("You won't make it off the island because you're dead!");
-                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine("-----------------------------------------------------");
                 Console.WriteLine("Better luck next time!");
+                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxx");
                 Environment.Exit(0);
                 
             }
             else
             {
                 Console.WriteLine("You've had a great night of rest, but you're getting pretty hungry!");
-                Console.WriteLine("You can either head into the forest to look for berries or find supplies and craft a spear to hunt for fish and game.");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("You can either head into the forest to look for berries...");
+                Console.WriteLine("OR you can find supplies and craft a spear to hunt for fish and game.");
+                Console.WriteLine(".....................................................................");
                 Console.WriteLine("Type 'a' if you would like to look for berries or 'b' to craft a spear and hunt:");
                 input = Console.ReadLine();
                 if (input == "a")
                 {
-                    Console.WriteLine("Oh no! The berries you found were poison berries! You won't make it off the island because you're dead.");
+                    Console.WriteLine("Oh no! The berries you found were poison berries!");
+                    Console.WriteLine("You won't make it off the island because you're dead.");
+                    Console.WriteLine("-----------------------------------------------------");
                     Console.WriteLine("Better luck next time!");
+                    Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxx");
                     Environment.Exit(0);   
                 }
                 else
                 {
                     Console.WriteLine("It was tricky to catch the fish, but you did it!");
                     Console.WriteLine("I think it's about time to look for a way off this island!");
-                    Console.WriteLine("You can either try to build your own raft or you can build a large S.O.S. sign on the beach.");
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    Console.WriteLine("You can either try to build your own raft...");
+                    Console.WriteLine("or you can build a large S.O.S. sign on the beach.");
+                    Console.WriteLine("..................................................");
                     Console.WriteLine("Type 'a' if you would like to build a raft or 'b' to build an S.O.S. sign:");
                     input = Console.ReadLine();
                     if (input == "a")
                     {
-                        Console.WriteLine("Your boat came together pretty quickly, but you didn't think about how long it would stay afloat... You make it to open waters and your boat sinks. You're dead.");
+                        Console.WriteLine("Your boat came together pretty quickly,");
+                        Console.WriteLine("but you didn't think about how long it would stay afloat!");
+                        Console.WriteLine("You make it to open waters and your boat sinks. You're dead.");
+                        Console.WriteLine("------------------------------------------------------------");
                         Console.WriteLine("Better luck next time!");
+                        Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxx");
                         Environment.Exit(0);                  
                     }
                     else
                     {
                         Console.WriteLine("The sticks and leaves you found look great on the beach!");
                         Console.WriteLine("A passing plane sees your S.O.S. and saves you!");
+                        Console.WriteLine("***********************************************");
                         Console.WriteLine("Congratulations you've made it off the island!!");
+                        Console.WriteLine("***********************************************");
                     }
                 }
             }
